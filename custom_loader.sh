b1eff4d40aac074fc119f69c90a9249036798453
RoySubs <roysubs@hotmail.com>
2021-10-06 11:21:57 +0200

    Update

--- custom_loader.sh
+++ custom_loader.sh
@@ -2540,21 +2540,22 @@ fi
 #   e.g. die () { test -n "$@" && echo "$@"; exit 1 }   The script might complete but the error will be seen.
 #   To make the dumb parser happy:   die () { test -n "$@" && echo "$@"; exit 1; }     (i.e. add the ";" at the end)
 # - Use https://www.shellcheck.net/# to try and debug issues
-#
-# Tip: use trap to debug (if your script is on the large side...)
-# e.g.
-# set -x
-# trap read debug
-# 
-# The 'trap' command is a way to debug your scripts by esentially breaking after every line.
-# A fuller discussion is here: 
-# hppts://stackoverflow.com/questions/951336/how-to-debug-a-bash-script/45096876#45096876 
-#
-# An alternative for handling line endings:
-# export SHELLOPTS
-# set -o igncr
-# Put into .bashrc or .bash_profile and then do not need to run unix2dos
-
-# Default LS_COLORS (on Ubuntu)
-# Original di is 01;34 then is overwritten by 0;94 later
-# rs=0:di=01;34:ln=01;36:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:mi=00:su=37;41:sg=30;43:ca=30;41:tw=30;42:ow=34;42:st=37;44:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arc=01;31:*.arj=01;31:*.taz=01;31:*.lha=01;31:*.lz4=01;31:*.lzh=01;31:*.lzma=01;31:*.tlz=01;31:*.txz=01;31:*.tzo=01;31:*.t7z=01;31:*.zip=01;31:*.z=01;31:*.dz=01;31:*.gz=01;31:*.lrz=01;31:*.lz=01;31:*.lzo=01;31:*.xz=01;31:*.zst=01;31:*.tzst=01;31:*.bz2=01;31:*.bz=01;31:*.tbz=01;31:*.tbz2=01;31:*.tz=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.war=01;31:*.ear=01;31:*.sar=01;31:*.rar=01;31:*.alz=01;31:*.ace=01;31:*.zoo=01;31:*.cpio=01;31:*.7z=01;31:*.rz=01;31:*.cab=01;31:*.wim=01;31:*.swm=01;31:*.dwm=01;31:*.esd=01;31:*.jpg=01;35:*.jpeg=01;35:*.mjpg=01;35:*.mjpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.svg=01;35:*.svgz=01;35:*.mng=01;35:*.pcx=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.m2v=01;35:*.mkv=01;35:*.webm=01;35:*.ogm=01;35:*.mp4=01;35:*.m4v=01;35:*.mp4v=01;35:*.vob=01;35:*.qt=01;35:*.nuv=01;35:*.wmv=01;35:*.asf=01;35:*.rm=01;35:*.rmvb=01;35:*.flc=01;35:*.avi=01;35:*.fli=01;35:*.flv=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.yuv=01;35:*.cgm=01;35:*.emf=01;35:*.ogv=01;35:*.ogx=01;35:*.aac=00;36:*.au=00;36:*.flac=00;36:*.m4a=00;36:*.mid=00;36:*.midi=00;36:*.mka=00;36:*.mp3=00;36:*.mpc=00;36:*.ogg=00;36:*.ra=00;36:*.wav=00;36:*.oga=00;36:*.opus=00;36:*.spx=00;36:*.xspf=00;36::di=0;94
+# - https://unix.stackexchange.com/questions/672004/bash-debug-options/672058#672058
+#   I found this simple shell breakpoint-debugger in a german shell-scripting book. You add the two following lines where ever you want to start debugging and step through by pressing enter or evaulate variables and functions at runtime.
+#      read var    # set simple breakpoint
+#      echo -e "dbg> \c"; read cmd; eval $cmd
+#      run script
+#   $ dbg> doSomethingFunction
+#   $ dbg> echo $SOME_VAR
+# - Alternatively using set -x and modifying the prompt variable PS4 has helped me in the past.
+#   PS4='+ ${BASH_SOURCE##*/}.${LINENO} ${FUNCNAME}: '
+# - Tip: use trap to debug (if your script is on the large side...)
+#   e.g.     set -x
+#   trap read debug
+#   The 'trap' command is a way to debug your scripts by esentially breaking after every line.
+#   A fuller discussion is here: 
+#   hppts://stackoverflow.com/questions/951336/how-to-debug-a-bash-script/45096876#45096876 
+# - An alternative for handling line endings:
+#   export SHELLOPTS
+#   set -o igncr
+#   Put into .bashrc or .bash_profile and then do not need to run unix2dos
